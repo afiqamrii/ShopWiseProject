ce99d7b45d4946136c4a97a9b80828cda8a503a5
Afiq Amri <120144945+afiqamrii@users.noreply.github.com>
2024-06-23 15:57:41 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -39,7 +39,7 @@ function signIn(){
 
     let params = {
         "client_id" : "998811034715-95e7b036slgl0p114tnshhr4r0bb2ln3.apps.googleusercontent.com",
-        "redirect_uri" : "https://shop-wise-projects.vercel.app/ShopWise-Projects/shopping.html",
+        "redirect_uri" : "https://shop-wise-project-V1.vercel.app/ShopWise-Projects/shopping.html",
         "response_type" : "token",
         "scope" : "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly",
         "include_granted_scopes" : "true",
@@ -108,7 +108,7 @@ function logout() {
 
     //Redirect User to Homepage
     .then((data) => {
-        location.href = "https://shop-wise-projects.vercel.app/ShopWise-Projects/index.html"
+        location.href = "https://shop-wise-project-V1.vercel.app/ShopWise-Projects/index.html"
     })
 
 }
